bf88f10002b45243a2d8d4252679d4603cd9fc11
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-07-20 18:59:02 +0530

    Let keyword enforce the scope within the block like the other languages

---
+++ Block_scope/block_scope_with_let.js
@@ -0,0 +1,50 @@
+/**
+ * Created by kk on 20/7/17.
+ */
+function foo(x,y){
+    if(x>y){
+        let tmp = x;
+        x = y;
+        y = tmp;
+
+        console.log(tmp);
+    }
+    console.log(tmp+"outside");
+    for(let i=0; i<10; i++){
+        console.log(i);
+    }
+    console.log(i+"outside");
+}
+
+foo(8,6) //tmp is not defined
+
+
+function foo(x,y){
+    if(x>y){
+        var tmp = x;
+        x = y;
+        y = tmp;
+
+        console.log(tmp);
+    }
+    console.log(tmp+"outside");
+    for(var i=0; i<10; i++){
+        console.log(i);
+    }
+    console.log(i+"outside");
+}
+
+foo(8,6)  /*
+  8
+  8outside
+  0
+  1
+  2
+  3
+  4
+  5
+  6
+  7
+  8
+  9
+ 10outside*/
