6e868b518d22132cefa84731bcd1910ce8091897
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-07-20 15:57:33 +0530

    Using bind function binding the current scope to anonymous function block

--- arrow-functions/arrow-function-prebuilt-bind.js
+++ arrow-functions/arrow-function-prebuilt-bind.js
@@ -1,10 +1,10 @@
 var obj = {
     id: 42,
     foo: function foo() {
-        var self = this;
+
         setTimeout(function(){
-            console.log( self.id );
-        },1000)
+            console.log( this.id );
+        }.bind(this),1000)
     }
 }
 obj.foo(); // It will get 42
