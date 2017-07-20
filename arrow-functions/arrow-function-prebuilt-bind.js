2bb7ae158b45ff7439c615c43b4c8db7cdf97497
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-07-20 16:02:33 +0530

    Arrow function inbuilt bind to current scope with out using bind() method.

--- arrow-functions/arrow-function-prebuilt-bind.js
+++ arrow-functions/arrow-function-prebuilt-bind.js
@@ -1,10 +1,9 @@
 var obj = {
     id: 42,
     foo: function foo() {
-
-        setTimeout(function(){
-            console.log( this.id );
-        }.bind(this),1000)
+        setTimeout(() =>
+            console.log( this.id )
+            ,1000)
     }
 }
 obj.foo(); // It will get 42
