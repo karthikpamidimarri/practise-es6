8df68c8dc6859fee05acba9de19332435beb14f2
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-07-21 10:48:16 +0530

    Full version of default value of idiom is

--- Default/default.js
+++ Default/default.js
@@ -11,3 +11,14 @@ console.log(foo(null));  //42
 console.log(foo(undefined));  //42
 console.log(foo(5));  //5
 
+// Full version of default value of idiom is
+
+function foof(x) {
+    x = x != undefined ? x : 42;
+    console.log(x);
+}
+
+console.log(foo(0));  //42
+console.log(foo(null));  //42
+console.log(foo(undefined));  //42
+console.log(foo(5));  //5
